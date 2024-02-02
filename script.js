259d327ed40121b3f0068de92352f151803ddc8d
ｊＡｉ♡ <143520569+ittsjai@users.noreply.github.com>
2024-02-02 09:07:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".loading-container").style.display = "none";
   }, 5000);
 });
-
-var links = document.querySelectorAll("a");
-links.forEach(function (link) {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = link.href;
-  });
-});
