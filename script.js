1c84284e14c4e0a7c07d2de2994962a7f74f851a
ｊＡｉ♡ <143520569+ittsjai@users.noreply.github.com>
2024-02-02 08:59:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.loading-container').style.display = 'flex';
-  setTimeout(function() {
-    document.querySelector('.loading-container').style.display = 'none';
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".loading-container").style.display = "flex";
+  setTimeout(function () {
+    document.querySelector(".loading-container").style.display = "none";
   }, 5000);
 });
-/*
-var links = document.querySelectorAll('a'); links.forEach(function(link) {
- link.addEventListener('click', function (event) {
- event.preventDefault();
- setTimeout(function () {
-window.location.href = event.target.href;
-                }, 2000);
-            });
-        });
-*/
+
+var links = document.querySelectorAll("a");
+links.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    setTimeout(function () {
+      window.location.href = link.href;
+    }, 500);
+  });
+});
