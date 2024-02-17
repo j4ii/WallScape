e789cc59ac6556f6797a8fa4cfc48a0476663f72
ｊＡｉ <143520569+ittsjai@users.noreply.github.com>
2024-02-17 21:28:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".loading-container").style.display = "flex";
   setTimeout(function () {
     document.querySelector(".loading-container").style.display = "none";
-  }, 2500);
+  }, 500);
 });
+const list = document.querySelectorAll(".wallsCategoryLink");
+function activeLink() {
+  const activeIndex = Array.from(list).indexOf(this);
+  localStorage.setItem("activeIndex", activeIndex);
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
+const activeIndex = localStorage.getItem("activeIndex");
+if (activeIndex !== null) {
+  list[activeIndex].classList.add("active");
+}
