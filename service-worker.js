f712997602c7633f4ada798f8c3a19a2c05a04a1
ｊＡｉ <143520569+ittsjai@users.noreply.github.com>
2024-03-08 22:00:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
 const CACHE_NAME = "WallScape-cache-v1";
 const urlsToCache = [
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/logo/icon.png",
+  "https://ittsjai.github.io/WallScape/index.html",
+  "https://ittsjai.github.io/WallScape/style.css",
+  "https://ittsjai.github.io/WallScape/script.js",
+  "https://ittsjai.github.io/WallScape/logo/icon.png",
 ];
-
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -15,27 +14,21 @@ self.addEventListener("install", function (event) {
     })
   );
 });
-
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       if (response) {
         return response;
       }
-
       var fetchRequest = event.request.clone();
-
       return fetch(fetchRequest).then(function (response) {
         if (!response || response.status !== 200 || response.type !== "basic") {
           return response;
         }
-
         var responseToCache = response.clone();
-
         caches.open(CACHE_NAME).then(function (cache) {
           cache.put(event.request, responseToCache);
         });
-
         return response;
       }).catch(function(error) {
         console.error('Failed to fetch resource:', error);
